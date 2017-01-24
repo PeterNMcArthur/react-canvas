65efce814b6a55ade16a9947c396e7490b75a468
Rodin van der Hart <rodin.hart@concentra.co.uk>
2017-01-24 10:39:16 +0000

    Added translate to coracle canvas example

--- rodin-lowleveltest/index.js
+++ rodin-lowleveltest/index.js
@@ -12,6 +12,15 @@ canvas.render([
       width: 200,
       height: 90,
       fill: 0xFF7F3F
+    },
+    {
+      transforms: [
+        {
+          transforms: "Translate",
+          x: 100,
+          y: 10
+        }
+      ]
     }
   ]
 ])
